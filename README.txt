f2d8a9c4e1b29295d851d4b9cc5169e4d170e8b3
NIYAS AHAMED . N <niyazashu.templates007@gmail.com>
2025-04-24 22:11:49 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,19 @@
+# countdown HTML Template
+
+A minimal, clean Coming Soon page with a 2-day dynamic countdown timer.
+
+## Features
+- Fully responsive layout
+- JavaScript-based live countdown
+- Easy customization
+- Perfect for product or portfolio launches
+
+## How to Use
+1. Download or clone this repo
+2. Replace logo and background image inside `assets/`
+3. Open `index.html` in any browser
+4. To edit the countdown time, go to `script.js`
+
+
+
+**Created by Niyaz Ashu**
---
+++ script.js
@@ -0,0 +1,25 @@
+
+var now = new Date().getTime();
+
+
+var countDownDate = now + (2 * 24 * 60 * 60 * 1000); // 2 days from now
+
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "We are live!";
+  }
+}, 1000);
